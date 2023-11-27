3f100b5abed3a9dc89e6bfb4ae9278b904b2a9e7
Schaper, Markus (SE-A/34) <markus.schaper1@volkswagen.de>
2023-11-27 14:29:12 +0100

    Added cd feature in feature branch

--- Bash/src/commands/Cd.java
+++ Bash/src/commands/Cd.java
@@ -1,11 +1,27 @@
 package commands;
 
+import java.io.File;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
 public class Cd {
-    
 
+    private Cd() {
 
-    public static void cd(String path) {
+    }
 
+    public static void cd(String path, String currentDirectory) {
+        File currentDir = new File(currentDirectory);
+        String[] fileList = currentDir.list();
+        if (Arrays.asList(fileList).contains(path)) {
+            System.setProperty("user.dir", new File(path).getAbsolutePath());
+            System.out.println("Directory changed to " + currentDirectory + "/" + path);
+        } else if (path.equals("..")) {
+            String parent = Paths.get(System.getProperty("user.dir")).getParent().toAbsolutePath().toString();
+            System.setProperty("user.dir", parent);
+        } else {
+            System.out.println("No such file or directory");
+        }
     }
 
 }
